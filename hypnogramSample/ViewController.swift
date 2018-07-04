b543bb2281850d75ca5d4d704ed101c66225ae1d
Adrien Catanas <adrien@rythm.co>
2018-07-04 16:26:04 +0200

    Initial Commit

---
+++ hypnogramSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hypnogramSample
+//
+//  Created by Adrien Catanas on 04/07/2018.
+//  Copyright © 2018 Rythm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
